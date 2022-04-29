2b2530496ff04f6c99d865620ebf9712dfd3cfe2
Bouttes Hugo <ZV2AOJ@ad.insee.intra>
2022-04-29 11:30:21 +0200

    rappatriement de poc stromae

--- src/main/java/insee/stage/demo/SpringBootMongodbTutorialApplication.java
+++ src/main/java/insee/stage/demo/SpringBootMongodbTutorialApplication.java
@@ -1,18 +1,19 @@
 package insee.stage.demo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.BasicErrorController;
 import org.springframework.context.annotation.Import;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
-@EnableSwagger2
-@Import(SpringFoxConfig.class)
 public class SpringBootMongodbTutorialApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootMongodbTutorialApplication.class, args);
     }
+
 }
