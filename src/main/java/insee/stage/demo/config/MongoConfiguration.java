48b3d090e4ed2cc5b542b4efcb9ea74338014e64
HBouttes <103732523+BouttesINSEE@users.noreply.github.com>
2022-05-13 13:45:29 +0200

    dummy changes

---
+++ src/main/java/insee/stage/demo/config/MongoConfiguration.java
@@ -0,0 +1,29 @@
+package insee.stage.demo.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoDatabase;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+//TODO handle case when error occurred with connexion to db
+
+@Configuration
+public class MongoConfiguration {
+
+    @Value("${mongodb.database}")
+    private String database ;
+    @Value("${mongodb.uri}")
+    private String mongoUri ;
+
+    private MongoClient mongoClient() {
+        return MongoClients.create(mongoUri);
+    }
+
+    @Bean(name = "mongoDatabase")
+    public MongoDatabase mongoDatabase() {
+        return mongoClient().getDatabase(database);
+    }
+
+}
