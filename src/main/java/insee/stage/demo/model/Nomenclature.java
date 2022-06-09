3ba604d3a95f5d1a2a07a1f4afaa78e7f7c42ffb
HBouttes <103732523+BouttesINSEE@users.noreply.github.com>
2022-06-09 10:10:35 +0200

    change Nomenclature.java

--- src/main/java/insee/stage/demo/model/Nomenclature.java
+++ src/main/java/insee/stage/demo/model/Nomenclature.java
@@ -1,98 +1,32 @@
-package insee.stage.demo.model;
+package insee.stage.demo.controller;
 
-import org.json.JSONObject;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Field;
+import insee.stage.demo.model.Nomenclature;
+import insee.stage.demo.service.NomenclatureService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-public class Nomenclature {
-    @Id
-    private String id;
-    @Field(name = "reg")
-    private String reg;
-    @Field(name = "cheflieu")
-    private String cheflieu;
-    @Field(name = "libelle")
-    private String libelle;
-    @Field(name = "tncc")
-    private String tncc;
-    @Field(name = "nccenr")
-    private String nccenr;
-    @Field(name = "ncc")
-    private String ncc;
+import java.util.List;
 
-    private JSONObject nomenclature;
+    @RestController
+    @RequestMapping("/api/nomenclature")
+    public class NomenclatureController {
+        private final NomenclatureService nomenclatureService;
 
-    public Nomenclature(String id, String reg, String cheflieu, String libelle, String tncc, String nccenr, String ncc) {
-        this.id = id;
-        this.reg = reg;
-        this.cheflieu = cheflieu;
-        this.libelle = libelle;
-        this.tncc = tncc;
-        this.nccenr = nccenr;
-        this.ncc = ncc;
+        public NomenclatureController(NomenclatureService nomenclatureService){
+            this.nomenclatureService = nomenclatureService;
+        }
 
-        this.nomenclature.put("id",id);
-        this.nomenclature.put("reg",reg);
-        this.nomenclature.put("cheflieu",cheflieu);
-        this.nomenclature.put("libelle",libelle);
-        this.nomenclature.put("tncc",tncc);
-        this.nomenclature.put("nccenr",nccenr);
-        this.nomenclature.put("ncc",ncc);
+        @GetMapping
+        public ResponseEntity<List<Nomenclature>> getAllExpenses() {
+            return ResponseEntity.ok(nomenclatureService.getAllNomenclature());
+        }
+
+        @GetMapping("/{id}")
+        public ResponseEntity<Nomenclature> getNomenclatureById(@PathVariable String id) {
+            return ResponseEntity.ok(nomenclatureService.getNomenclatureById(id));
+        }
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getReg() {
-        return reg;
-    }
-
-    public void setReg(String reg) {
-        this.reg = reg;
-    }
-
-    public String getCheflieu() {
-        return cheflieu;
-    }
-
-    public void setCheflieu(String cheflieu) {
-        this.cheflieu = cheflieu;
-    }
-
-    public String getLibelle() {
-        return libelle;
-    }
-
-    public void setLibelle(String libelle) {
-        this.libelle = libelle;
-    }
-
-    public String getTncc() {
-        return tncc;
-    }
-
-    public void setTncc(String tncc) {
-        this.tncc = tncc;
-    }
-
-    public String getNccenr() {
-        return nccenr;
-    }
-
-    public void setNccenr(String nccenr) {
-        this.nccenr = nccenr;
-    }
-
-    public String getNcc() {
-        return ncc;
-    }
-
-    public void setNcc(String ncc) {
-        this.ncc = ncc;
-    }
-}
